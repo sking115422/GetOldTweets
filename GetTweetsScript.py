53bcd762964302d02a08fb2738bfa79dea20f61e
sking115422 <spencerd.king@gmail.com>
2021-12-26 22:18:20 +0000

    added more error handling to script

--- GetTweetsScript.py
+++ GetTweetsScript.py
@@ -86,31 +86,39 @@ try:
             
             # Loop through each response object
             for response in tweets_list:
-                # Take all of the users, and put them into a dictionary of dictionaries with the info we want to keep
-                for user in response.includes['users']:
-                    user_dict[user.id] = {'username': user.username, 
-                                        'followers': user.public_metrics['followers_count'],
-                                        'tweets': user.public_metrics['tweet_count'],
-                                        'description': user.description,
-                                        'location': user.location
-                                        }
-                for tweet in response.data:
-                    # For each tweet, find the author's information
-                    author_info = user_dict[tweet.author_id]
-                    # Put all of the information we want to keep in a single dictionary for each tweet
-                    result.append({'author_id': tweet.author_id, 
-                                'username': author_info['username'],
-                                'author_followers': author_info['followers'],
-                                'author_tweets': author_info['tweets'],
-                                'author_description': author_info['description'],
-                                'author_location': author_info['location'],
-                                'text': tweet.text,
-                                'created_at': tweet.created_at,
-                                'retweets': tweet.public_metrics['retweet_count'],
-                                'replies': tweet.public_metrics['reply_count'],
-                                'likes': tweet.public_metrics['like_count'],
-                                'quote_count': tweet.public_metrics['quote_count']
-                                })
+                
+                try:
+                    # Take all of the users, and put them into a dictionary of dictionaries with the info we want to keep
+                    for user in response.includes['users']:
+                        user_dict[user.id] = {'username': user.username, 
+                                            'followers': user.public_metrics['followers_count'],
+                                            'tweets': user.public_metrics['tweet_count'],
+                                            'description': user.description,
+                                            'location': user.location
+                                            }
+                    for tweet in response.data:
+                        # For each tweet, find the author's information
+                        author_info = user_dict[tweet.author_id]
+                        # Put all of the information we want to keep in a single dictionary for each tweet
+                        result.append({'author_id': tweet.author_id, 
+                                    'username': author_info['username'],
+                                    'author_followers': author_info['followers'],
+                                    'author_tweets': author_info['tweets'],
+                                    'author_description': author_info['description'],
+                                    'author_location': author_info['location'],
+                                    'text': tweet.text,
+                                    'created_at': tweet.created_at,
+                                    'retweets': tweet.public_metrics['retweet_count'],
+                                    'replies': tweet.public_metrics['reply_count'],
+                                    'likes': tweet.public_metrics['like_count'],
+                                    'quote_count': tweet.public_metrics['quote_count']
+                                    })
+                
+                except Exception as e:
+                    f= open('./logs/log_' + data_set_name + '.txt', 'a')
+                    f.write("Response Skipped Error Occured: " + str(e))
+                    f.close()
+
 
             
             # Change this list of dictionaries into a dataframe
